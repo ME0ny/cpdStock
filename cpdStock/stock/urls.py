0a0ea65b3de8d511e1f0febac0f997a98c1d5459
ME0ny <ipatov.987654321@gmail.com>
2020-09-20 17:56:27 +1000

    search in one site

--- cpdStock/stock/urls.py
+++ cpdStock/stock/urls.py
@@ -6,5 +6,4 @@ from .views import HomePageView, SearchResultsView
 urlpatterns = [
     path('admin/', admin.site.urls),  
     path('search/', SearchResultsView.as_view(), name='search_results'),     
-    path('', HomePageView.as_view(), name='home'),
 ]
--- cpdStock/stock/views.py
+++ cpdStock/stock/views.py
@@ -4,30 +4,29 @@ from django.views.generic import TemplateView, ListView
 from django.db.models import Q
 # Create your views here.
 
-class HomePageView(TemplateView):
-    template_name = 'home.html'
-
 class SearchResultsView(ListView):
     model = Stuff
     template_name = 'search_results.html'
     def get_queryset(self): # новый
         query = self.request.GET
-        queryM={}
-        queryM['X']=int(query['X'])
-        queryM['Y']=int(query['Y'])
-        queryM['Z']=int(query['Z'])
-        if(query['q']!=''):
-            object_list = Stuff.objects.filter(
-                Q(tittle__icontains=query['q']))
-        elif(query['q']=='' and queryM['X']!=0 and queryM['Y']==0):
-            object_list = Stuff.objects.filter(
-                Q(position_X__icontains=queryM['X']))
-        elif(query['q']=='' and queryM['X']!=0 and queryM['Y']!=0 and queryM['Z']==0):
-            object_list = Stuff.objects.filter(
-                Q(position_X__icontains=queryM['X'] & Q(position_Y__icontains=queryM['Y'])))
-        elif(query['q']=='' and queryM['X']!=0 and queryM['Y']!=0 and queryM['Z']!=0):
-            object_list = Stuff.objects.filter(
-                Q(position_X__icontains=queryM['X'] & Q(position_Y__icontains=queryM['Y']) & Q(position_Z__icontains=queryM['Z'])))
-        else:
-            object_list = Stuff.objects.all()
-        return object_list
+        if(len(query)>0):
+            queryM={}
+            queryM['X']=int(query['X'])
+            queryM['Y']=int(query['Y'])
+            queryM['Z']=int(query['Z'])
+            if(query['q']!='' and queryM['X']==0):
+                object_list = Stuff.objects.filter(
+                    Q(tittle__icontains=query['q']))
+            elif(query['q']=='' and queryM['X']!=0 and queryM['Y']==0):
+                object_list = Stuff.objects.filter(
+                    Q(position_X__icontains=queryM['X']))
+            elif(query['q']=='' and queryM['X']!=0 and queryM['Y']!=0 and queryM['Z']==0):
+                object_list = Stuff.objects.filter(
+                    Q(position_X__icontains=queryM['X'] & Q(position_Y__icontains=queryM['Y'])))
+            elif(query['q']=='' and queryM['X']!=0 and queryM['Y']!=0 and queryM['Z']!=0):
+                object_list = Stuff.objects.filter(
+                    Q(position_X__icontains=queryM['X'] & Q(position_Y__icontains=queryM['Y']) & Q(position_Z__icontains=queryM['Z'])))
+            else:
+                object_list = Stuff.objects.all()
+            return object_list
+        return None
